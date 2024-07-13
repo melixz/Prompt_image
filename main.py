573969dd1a956c07f2e8bf2e97188c1aae741979
Vlad <dr.melix@gmail.com>
2024-07-13 17:11:34 +0300

    Добавлена обработка исключений, кэширование и аннотации типов для улучшения кода

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import replicate
+from typing import Dict, Any
 
 st.markdown("# :red[Prompt and image]")
 
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
 
 
-def configure_sidebar():
+def configure_sidebar() -> Dict[str, Any]:
     with st.sidebar:
         with st.form("my_form"):
             width = st.number_input("Ширина картинки", min_value=256, max_value=2048, value=1024)
@@ -22,25 +23,30 @@ def configure_sidebar():
         }
 
 
-def main_page(
-        width: int,
-        height: int,
-        prompt: str,
-        submitted: bool,
-):
+@st.cache_data
+def generate_image(width: int, height: int, prompt: str) -> str:
+    try:
+        result = replicate.run(
+            "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            input={
+                "width": width,
+                "height": height,
+                "prompt": prompt,
+            }
+        )
+        return result[0]
+    except Exception as e:
+        st.error(f"Произошла ошибка при генерации изображения: {e}")
+        return ""
+
+
+def main_page(width: int, height: int, prompt: str, submitted: bool):
     if submitted:
         with st.spinner("В процессе загрузки"):
-            result = replicate.run(
-                "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
-                input={
-                    "width": width,
-                    "height": height,
-                    "prompt": prompt,
-                }
-            )
-            image = result[0]
-            with st.container():
-                st.image(image, caption="Ваша картинка")
+            image_url = generate_image(width, height, prompt)
+            if image_url:
+                with st.container():
+                    st.image(image_url, caption="Ваша картинка")
 
 
 def main():
